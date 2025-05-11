6349ec7911f1954c0f27c98831e57ba2135d4fb7
Michael Linka <michael.linka@ataccama.com>
2025-05-11 18:43:52 +0200

    day5 part 2 - spaghetti ftw

--- src/main/java/com/adventofcode/day5/Main.java
+++ src/main/java/com/adventofcode/day5/Main.java
@@ -3,8 +3,12 @@ package com.adventofcode.day5;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
 
@@ -12,8 +16,11 @@ public class Main {
 
     static List<String> linesOfOrderRules;
     static List<String> linesOfOrders;
+    static List<String[]> listOfOrdersWithIncorrectOrder = new ArrayList<>();
     static Set<Integer> nrOfOrdersWhichPassed = new HashSet<>();
     static Set<Integer> nrOfOrdersWhichFailed = new HashSet<>();
+    static Map<Integer, String[]> mapOfOrdersWithIncorrectOrder = new HashMap<>();
+    static boolean areAllFixed = false;
 
     public static void main(String[] args) {
 
@@ -22,6 +29,7 @@ public class Main {
 //        File file = new File("src/main/resources/input5part1-demo2.txt");
 
         int result = 0;
+        int resultPartTwo = 0;
 
         Scanner sc = getScanner(file);
 
@@ -52,6 +60,31 @@ public class Main {
 
         System.out.println("result: " + result);
         processLineOfOrders();
+
+
+        // PART 2
+        // reorder wrongly ordered orders and get sum of their middle nrs
+
+        goThroughIncorrectRules();
+        goThroughIncorrectRules();
+        goThroughIncorrectRules();
+        goThroughIncorrectRules();
+        goThroughIncorrectRules();
+
+        while (!areAllFixed) {
+            goThroughIncorrectRules();
+        }
+
+        System.out.println("done");
+//        TODO: count result when all orders in mapOfOrdersWithIncorrectOrder are reordered
+        for (String[] orderArr: mapOfOrdersWithIncorrectOrder.values()) {
+            resultPartTwo += Integer.parseInt(orderArr[orderArr.length / 2]);
+        }
+        System.out.println("Map:");
+        mapOfOrdersWithIncorrectOrder.forEach((key, value) -> System.out.println(key + " " + Arrays.toString(value)));
+
+
+        System.out.println("result part 2: " + resultPartTwo);
     }
 
     private static void goThroughRules() {
@@ -85,9 +118,79 @@ public class Main {
                 if (prevPosition != 0 && nextPosition != 0 && prevPosition > nextPosition) {
                     System.out.println("!!! ERROR !!!");
                     nrOfOrdersWhichFailed.add(j);
+
+                    String[] orderArrayCopy = orderArray.clone();
+
+                    int tmp = Integer.parseInt(orderArrayCopy[prevPosition - 1]);
+                    orderArrayCopy[prevPosition - 1] = orderArrayCopy[nextPosition - 1];
+                    orderArrayCopy[nextPosition - 1] = String.valueOf(tmp);
+
+                    System.out.println("This should be corrected: " + Arrays.toString(orderArrayCopy));
+
+                    listOfOrdersWithIncorrectOrder.add(orderArrayCopy);
+                    mapOfOrdersWithIncorrectOrder.put(j, orderArrayCopy);
                 }
                 nrOfOrdersWhichPassed.add(j);
                 j++;
+            }
+        }
+    }
+
+    private static void goThroughIncorrectRules() {
+        Collections.shuffle(linesOfOrderRules);
+
+        for (String rules : linesOfOrderRules) {
+            System.out.println("checking rule: " + rules);
+
+            int previousPageNr = Integer.parseInt(rules.split("\\|")[0]);
+            int nextPageNr = Integer.parseInt(rules.split("\\|")[1]);
+
+            int j = 0;
+            int nrOfErrors = 0;
+
+            Map<Integer, String[]> newMapOfOrdersWithIncorrectOrder = new HashMap<>();
+
+            for (String[] order : mapOfOrdersWithIncorrectOrder.values()) {
+                System.out.println("checking order: " + Arrays.toString(order));
+
+                int i = 1;
+                int prevPosition = 0;
+                int nextPosition = 0;
+                for (String nr : order) {
+                    if (Integer.parseInt(nr) == previousPageNr) {
+                        prevPosition = i;
+                        System.out.println("first page nr. present, position: " + i);
+                    }
+                    if (Integer.parseInt(nr) == nextPageNr) {
+                        nextPosition = i;
+                        System.out.println("second page nr. present, position: " + i);
+                    }
+                    i++;
+                }
+                if (prevPosition != 0 && nextPosition != 0 && prevPosition > nextPosition) {
+                    System.out.println("!!! ERROR !!!");
+                    nrOfErrors++;
+                    nrOfOrdersWhichFailed.add(j);
+
+                    String[] orderArrayCopy = order.clone();
+
+                    int tmp = Integer.parseInt(orderArrayCopy[prevPosition - 1]);
+                    orderArrayCopy[prevPosition - 1] = orderArrayCopy[nextPosition - 1];
+                    orderArrayCopy[nextPosition - 1] = String.valueOf(tmp);
+
+                    System.out.println("This should be corrected: " + Arrays.toString(orderArrayCopy));
+
+                    listOfOrdersWithIncorrectOrder.add(orderArrayCopy);
+                    newMapOfOrdersWithIncorrectOrder.put(j, orderArrayCopy);
+                } else {
+                    newMapOfOrdersWithIncorrectOrder.put(j, order);
+                }
+                nrOfOrdersWhichPassed.add(j);
+                j++;
+            }
+            mapOfOrdersWithIncorrectOrder = newMapOfOrdersWithIncorrectOrder;
+            if (nrOfErrors == 0) {
+                areAllFixed = true;
             }
         }
     }
